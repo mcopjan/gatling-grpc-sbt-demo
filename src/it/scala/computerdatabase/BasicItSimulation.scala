5ec37f86db3a3d3410231a2a4e9e1ded40dfb8fb
Martin Copjan <martin.copjan@derivco.co.uk>
2021-03-01 21:40:24 +0000

    add a simple test for a local exchange GRPC service

--- src/it/scala/computerdatabase/BasicItSimulation.scala
+++ src/it/scala/computerdatabase/BasicItSimulation.scala
@@ -1,26 +1,33 @@
-package computerdatabase
+import _root_.grpc.coingecko.coingecko.ExchangeServiceGrpc
+import _root_.grpc.coingecko.coingecko.StoreExchangeRequest
 
 import com.github.phisgr.gatling.grpc.Predef._
 import io.gatling.core.Predef._
 import io.grpc.health.v1.health.HealthCheckResponse.ServingStatus.SERVING
 import io.grpc.health.v1.health.{HealthCheckRequest, HealthGrpc}
+import com.github.phisgr.gatling.grpc.Predef._
+import com.github.phisgr.gatling.pb._
+// stringToExpression is hidden because we have $ in GrpcDsl
+import io.gatling.core.Predef.{stringToExpression => _, _}
+import io.gatling.core.session.Expression
+import io.grpc.Status
 
 import scala.concurrent.duration._
+import _root_.grpc.coingecko.coingecko.Exchange
 
 class BasicItSimulation extends Simulation {
 
-  val grpcConf = grpc(managedChannelBuilder("photoslibrary.googleapis.com", 443))
-  val request = grpc("request_1")
-    .rpc(HealthGrpc.METHOD_CHECK)
-    .payload(HealthCheckRequest.defaultInstance)
-    .extract(_.status.some)(_ is SERVING)
+  val grpcConf = grpc(managedChannelBuilder(name = "localhost", port = 6000).usePlaintext())
+    
 
-  val scn = scenario("Scenario Name") // A scenario is a chain of requests and pauses
-    .exec(request)
-    .pause(7.seconds)
-    .exec(request)
-    .exec(request)
-    .exec(request)
+  val request = grpc("store exchange request")
+    .rpc(ExchangeServiceGrpc.METHOD_STORE_EXCHANGE)
+    .payload(StoreExchangeRequest.defaultInstance.updateExpr(
+                _.exchange.name :~ "test"
+              ))
 
+
+  val scn = scenario("Store Exchange")
+    .exec(request)
   setUp(scn.inject(atOnceUsers(1)).protocols(grpcConf))
 }
